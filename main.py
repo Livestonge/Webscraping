2069f8227a1d0ac2528b1294480d2198b15f549a
Moussa Hassan <awaleh_h@yahoo.com>
2021-05-19 00:37:46 +0200

    New inputs

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import driver_object as driver
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    user = calenderFile.User("Paris", "Djibouti", calenderFile.Date(10, 5), calenderFile.Date(18, 8))
+    user = calenderFile.User("Paris", "new york", calenderFile.Date(10, 5), calenderFile.Date(6, 7))
     expedia_driver = driver.Expedia_scraper(user)
     expedia_driver.start_selenium()
 
