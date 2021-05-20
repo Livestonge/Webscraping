133c34b795d8c8df573854bcd6ffe59110d64ec6
Moussa Hassan <awaleh_h@yahoo.com>
2021-05-20 17:44:08 +0200

    The program is now asking for inputs from the user, user data is no longer hardcoded.

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-import classes as calenderFile
+import classes as calender_file
 import driver_object as driver
+import sys
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    user = calenderFile.User("Paris", "new york", calenderFile.Date(10, 5), calenderFile.Date(6, 7))
-    expedia_driver = driver.Expedia_scraper(user)
+    # Ask the user for input
+    departing_from = input("Where are you departing from: ")
+    destination = input("Where are you going to?")
+    month_dp = input("Which month are you leaving?")
+    day_dp = input("Which day are you leaving?")
+    month_arr = input("Which month are you coming back?")
+    day_arr = input("Which day are you coming back?")
+
+    # Since the program is using collections, we subtract 1 from the provided month number.
+    dp_date = calender_file.Date(day_dp, month_dp - 1)
+    arr_date = calender_file.Date(day_arr, month_arr - 1)
+
+    user = calender_file.User(departing_from, destination, dp_date, arr_date)
+    expedia_driver = driver.ExpediaScraper(user)
     expedia_driver.start_selenium()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
