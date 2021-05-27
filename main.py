3b30d313b0ee7377de3e0485ede81ec1f30d81d0
Moussa Hassan <awaleh_h@yahoo.com>
2021-05-27 15:54:01 +0200

    Refactored

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ if __name__ == '__main__':
     # Ask the user for input
     departing_from = input("Where are you departing from: ")
     destination = input("Where are you going to?")
-    month_dp = input("Which month are you leaving?")
-    day_dp = input("Which day are you leaving?")
-    month_arr = input("Which month are you coming back?")
-    day_arr = input("Which day are you coming back?")
+    month_dp = int(input("Which month are you leaving? f.ex 01 for January"))
+    day_dp = int(input("Which day are you leaving?"))
+    month_arr = int(input("Which month are you coming back? f.ex 02 for February"))
+    day_arr = int(input("Which day are you coming back?"))
 
     # Since the program is using collections, we subtract 1 from the provided month number.
     dp_date = calender_file.Date(day_dp, month_dp - 1)
